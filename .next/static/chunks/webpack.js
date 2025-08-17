e8c5ad9c1ac6cee78684bd8bc9a95ee1c5370f6e
afra.khalaj <fateme.m.a.khalaj@gmail.com>
2025-08-17 11:08:34 +0330

    changes applied

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4311b41d71094f75"; }
+/******/ 		__webpack_require__.h = function() { return "432988fc1b13711d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
